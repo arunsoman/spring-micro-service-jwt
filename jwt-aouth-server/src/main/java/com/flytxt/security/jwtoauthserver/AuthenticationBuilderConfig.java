883dbde0e383f80518dee28dfe92ea8ceb6f26e0
shiju.john <shiju.john@flytxt.com>
2016-12-22 14:02:14 +0530

    AutheticationType bean creation exception fix

--- jwt-aouth-server/src/main/java/com/flytxt/security/jwtoauthserver/AuthenticationBuilderConfig.java
+++ jwt-aouth-server/src/main/java/com/flytxt/security/jwtoauthserver/AuthenticationBuilderConfig.java
@@ -1,15 +1,21 @@
 package com.flytxt.security.jwtoauthserver;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 import com.flytxt.security.jwtoauthserver.authBuilder.AuthenticationType;
 
+@Configuration
+@EnableConfigurationProperties
 public class AuthenticationBuilderConfig {
     @Value("${authentication.AuthenticationBuilder}")
     private String authenticationBuilder;
     
     @Bean
+    @Primary
     public AuthenticationType getAuthenticationType() throws InstantiationException, IllegalAccessException, ClassNotFoundException{
     		return (AuthenticationType) Class.forName(authenticationBuilder).newInstance();
     }
--- jwt-aouth-server/src/main/java/com/flytxt/security/jwtoauthserver/authBuilder/LdapAuthenticationBuilder.java
+++ jwt-aouth-server/src/main/java/com/flytxt/security/jwtoauthserver/authBuilder/LdapAuthenticationBuilder.java
@@ -3,7 +3,6 @@ package com.flytxt.security.jwtoauthserver.authBuilder;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.ldap.authentication.ad.ActiveDirectoryLdapAuthenticationProvider;
 
